1c3838dc460410c43319a3d85579613d2c0e1eb0
Shmaiter <shmaither@gmail.com>
2024-02-15 16:52:35 -0600

    (feature): fire bullets from the ship and update its movement on every loop

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ class Settings:
         self.ship_speed = 4.5
 
         # Bullets settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 4.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
