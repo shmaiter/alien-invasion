0584919b6cd75657122bc8a1950c00010a1b3561
Shmaiter <shmaither@gmail.com>
2024-02-14 18:36:29 -0600

    (class): add the ship class, along with the image and method to show the element on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,24 +18,29 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color.
-        # self.bg = pygame.image.load("filename") # stars background to search
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                # print(event)
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
 
-                # Redraw the screen during each pass through the loop.
-                self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-                # Make the most recently drawn screen visible.
-                pygame.display.flip()
-                self.clock.tick(60)
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -9,4 +9,6 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (50, 50, 50)
+        self.bg_color = (135,206,235)
+        # Set the background color.
+        # self.bg = pygame.image.load("filename") # stars background to search
