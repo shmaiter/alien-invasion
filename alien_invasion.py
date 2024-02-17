c9a5d2150c719aac0fc508ecbe5ea85049ff07ac
Shmaiter <shmaither@gmail.com>
2024-02-17 11:39:28 -0600

    (feature): add left movement and edge recognition collition to change direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -109,8 +109,22 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_aliens(self):
-        """Update the position of all aliens in the fleet."""
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_screen(self):
