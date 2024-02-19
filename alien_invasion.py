07c12050ce88b920df77f6704fe6554a51959d1f
Shmaiter <shmaither@gmail.com>
2024-02-19 08:48:09 -0600

    (feature): delete bullets and aliens on collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _create_fleet(self):
         """Create a fleet of aliens"""
         # Create an alien and keep adding aliens until there's no room left.
